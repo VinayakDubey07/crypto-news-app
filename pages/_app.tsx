98fb07afcd73e699e89165519024c070760f55e0
VinayakDubey <vinayakdubey0108@gmail.com>
2022-12-08 16:00:00 +0530

    minor change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,8 @@
-import "tailwindcss/tailwind.css";
-import Head from "next/head";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
-  <Head>
-    <title>PortFox - News Portal</title>
-    <link rel="icon" href="/favicon.ico" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700;800&display=swap"
-      rel="stylesheet"
-    />
-  </Head>;
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
